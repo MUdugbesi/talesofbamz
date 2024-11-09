3309c3c33e5cee2f0441a76d42c4b73f557976b3
Marvelous <marvelous.udugbesi@icloud.com>
2024-11-09 12:01:42 +0000

    updated footer name

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = ({ handleContactPopOver }) => {
   return (
     <div className='w-[90%] mx-auto flex flex-col md:flex-row justify-center items-center mb-10 md:gap-3 mt-5'>
       <p className='text-white text-[12px] md:text-[16px] text-center'>
-        Developed and designed by @sparkle - content support by @TMZ
+        Developed and designed by @Sparkle - content support by @Teemah
       </p>
       <span
         className='hover:cursor-pointer text-[var(--bg2)] font-[500] max-sm:text-[14px] md:font-bold hover:scale-[1.05] contact'
